ff14405ebe42c4b89ea86cacb3b88e4579423709
GiteeAlisa <idolfu@163.com>
2019-07-09 00:41:30 +0800

    初始化打包

---
+++ topline-mobile/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>	
+    <router-view>app router</router-view>	
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data ()  {
+      return {
+
+      }
+    },
+    components: {
+
+    }
+  }
+</script>
+
+<style lang='less' scoped>
+  
+</style>
